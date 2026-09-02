df02191395c005fc24bc94b4a2a6f2903acbb93d
Yashvardhan Bhawnani <yashvardhanbhawnani@gmail.com>
2026-09-02 22:49:27 +0530

    add module code

---
+++ 01-node-foundations/thapa-technical/02_Modules/app.js
@@ -0,0 +1,8 @@
+// commonjs method
+// const add = require("./math");
+
+const { add, sub, mult, div } = require("./math");
+console.log(add(5, 10));
+console.log(sub(5, 10));
+console.log(mult(5, 10));
+console.log(div(5, 10));
