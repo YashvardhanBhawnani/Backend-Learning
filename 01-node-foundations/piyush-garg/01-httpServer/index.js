4d3fbaf7db8019312ca07d995b8dd030fe6b168f
Yashvardhan Bhawnani <yashvardhanbhawnani@gmail.com>
2026-09-04 21:27:30 +0530

    Add HttpServer Codes

---
+++ 01-node-foundations/piyush-garg/01-httpServer/index.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+  // console.log(req.headers);
+  // console.log("New Request Received!");
+  if (req.url === "/favicon.ico") return res.end("");
+
+  const log = `${Date.now()} : ${req.url} New Request Received✅ \n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
+
+  fs.appendFile("log.txt", log, (err, data) => {
+    if (err) console.error(err.message);
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("Welcome To Home Page");
+        break;
+      case "/about":
+        const name = myUrl.query.name;
+        res.end(`Hi, ${name}`);
+        break;
+      default:
+        res.end("Error 404 Not Found");
+    }
+  });
+  // res.end("Hello From Server");
+});
+
+server.listen(8000, () => {
+  console.log("Server Started! ✅ ");
+});
